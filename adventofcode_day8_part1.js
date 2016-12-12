dfe7cef05c033b8e03cb119ebf32e0075a566bf6
Tony Yu Zhao <tony.zhao@cpp.ca>
2016-12-12 16:21:41 -0500

    Added code to export sorted sparse matrix to file to be used in part 3

--- adventofcode_day8_part1.js
+++ adventofcode_day8_part1.js
@@ -3,7 +3,8 @@ const readline = require("readline")
 const MAX_ROW = 6, MAX_COL = 50
 
 const fill_rect = instr => {
-  const [num_col, num_row] = instr.split(/rect\s(\d+)x(\d+)/).filter(s => s.length>0)
+  const [num_col, num_row] = instr.split(/rect\s(\d+)x(\d+)/)
+    .filter(s => s.length>0).map(Number)
   for(var r=0; r<num_row && r<MAX_ROW; r++) {
     for(var c=0; c<num_col && c<MAX_COL; c++) {
       if(screen[r].indexOf(c) === -1) {
@@ -13,26 +14,28 @@ const fill_rect = instr => {
   }
 }
 
-const rotate_row =  instr => {
-  const [row, times] = instr.split(/rotate\srow\sy=(\d+)\sby\s(\d+)/).filter(s => s.length>0)
+const rotate_row = instr => {
+  const [row, times] = instr.split(/rotate\srow\sy=(\d+)\sby\s(\d+)/)
+    .filter(s => s.length>0).map(Number)
   let list_col = []
   for(var c=0; c<screen[row].length; c++) {
-    list_col.push((screen[row][c]+Number.parseInt(times))%MAX_COL)
+    list_col.push((screen[row][c]+times)%MAX_COL)
   }
   screen[row] = list_col
 }
 
-const rotate_col =  instr => {
-  const [col, times] = instr.split(/rotate\scolumn\sx=(\d+)\sby\s(\d+)/).filter(s => s.length>0)
+const rotate_col = instr => {
+  const [col, times] = instr.split(/rotate\scolumn\sx=(\d+)\sby\s(\d+)/)
+    .filter(s => s.length>0).map(Number)
   let list_row = []
   for(var r=0; r<screen.length; r++) {
-    if(screen[r].indexOf(Number.parseInt(col)) !== -1) {
-      list_row.push((Number.parseInt(r)+Number.parseInt(times))%MAX_ROW)
-      screen[r].splice(screen[r].indexOf(Number.parseInt(col)),1)
+    if(screen[r].indexOf(col) !== -1) {
+      list_row.push((r+times)%MAX_ROW)
+      screen[r].splice(screen[r].indexOf(col),1)
     }
   }
-  for(var r =0; r<list_row.length; r++) {
-    screen[list_row[r]].push(Number.parseInt(col))
+  for(var r=0; r<list_row.length; r++) {
+    screen[list_row[r]].push(col)
   }
 }
 
@@ -55,4 +58,18 @@ readline.createInterface({
   })
   .on("close", () => {
     console.log("number of pixels:", screen.reduce((sum, row) => sum+=row.length, 0))
+    const output_file = "sparse_matrix.txt"
+    fs.writeFile(output_file, '', err => {
+      if(err) {
+        console.log(err)
+      }
+    })
+    for(var r=0; r<screen.length; r++) {
+      screen[r].sort((obj1,obj2) => obj1-obj2)
+      fs.appendFile(output_file, screen[r] + '\n', err => {
+        if(err) {
+          console.log(err)
+        }
+      })
+    }
   })
