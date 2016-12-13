e9578939c25767c0ef090dfbad24962307c27a24
Tony Yu Zhao <tony.yu.zhao@gmail.com>
2016-12-13 03:14:44 -0500

    Refactored
    -more efficient regex processing

--- adventofcode_day7_part2.js
+++ adventofcode_day7_part2.js
@@ -1,7 +1,6 @@
 const fs = require("fs")
 const readline = require("readline")
-const regex = /(([a-z])(?!\2)([a-z])\2)/
-const bab_regex = /(\[[a-z]*([a-z])(?!\2)([a-z])\2[a-z]*\])/
+const aba_regex = /([a-z])(?!\1)([a-z])\1/g
 const hypernet_regex = /(\[[a-z]+\])/
 
 let num = 0
@@ -9,25 +8,19 @@ readline.createInterface({
     input: fs.createReadStream("input.txt")
   })
   .on("line", seq => {
-    let bab_seqs = seq.split(bab_regex)
-      .filter(s => bab_regex.test(s))
+    let hypernet = seq.split(hypernet_regex)
+      .filter(seq => hypernet_regex.test(seq))
       .join()
-    let outer_seqs = seq.split(hypernet_regex)
-      .filter(s => !hypernet_regex.test(s))
-      .join()
-    let aba_seqs = []
-    while(regex.test(outer_seqs)) {
-      let aba = outer_seqs.match(regex)[0]
-      aba_seqs.push(aba)
-      outer_seqs = outer_seqs.slice(outer_seqs.indexOf(aba)+1)
-    }
-    for(var i=0; i< aba_seqs.length; i++) {
-      let aba = aba_seqs[i]
-      if(bab_seqs.includes(aba[1]+aba[0]+aba[1])) {
-        num++
-        return
-      }
-    }
+    num += supernet = seq.split(hypernet_regex)
+      .filter(seq => !hypernet_regex.test(seq))
+      .reduce((abas,seq) => {
+        while((aba = aba_regex.exec(seq)) !== null) {
+          abas.push(aba[0])
+          aba_regex.lastIndex = aba.index+1
+        }
+        return abas
+      }, [])
+      .some( aba => hypernet.includes(aba[1]+aba[0]+aba[1]))
   })
   .on("close", () => {
     console.log("number of IPs supporting SSL:", num)
