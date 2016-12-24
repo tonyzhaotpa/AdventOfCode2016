2434139a6b18f5f7fd792a60a6bc455ffa339e0d
Tony Yu Zhao <tony.yu.zhao@gmail.com>
2016-12-23 21:40:35 -0500

    Added pruning
    Finds valid solutions, but they are only local minimums.  More
    heuristics needed.

--- adventofcode_day11_part1.js
+++ adventofcode_day11_part1.js
@@ -1,21 +1,32 @@
 const ms = f => f.match(/[a-z]/g) || []
 const gs = f => f.match(/[A-Z]/g) || []
 const last = steps => steps[steps.length - 1]
-const valid_moves = ["+HL","+hl","+Hh","+Ll","+H","+L","+h","+l","-H","-L","-h","-l"]
+const valid_moves = [
+"+ot","+op","+or","+oc",
+"+tp","+tr","+tc",
+"+pr","+pc",
+"+rc",
+"+OT","+OP","+OR","+OC",
+"+TP","+TR","+TC",
+"+PR","+PC",
+"+RC",
+"+Oo","+Tt","+Pp","+Rr","+Cc",
+"+O","+T","+P","+R","+C","+o","+t","+p","+r","+c",
+"-O","-T","-P","-R","-C","-o","-t","-p","-r","-c"]
 
 const root = p => [p]
 const reject = steps => {
   for(var i = 0; i < steps.length - 1; i++) {
-    if(steps[i].diag.toString() == last(steps).diag.toString()) {
+    if(steps[i].diag.every((f,index) => ms(f).length == ms(last(steps).diag[index]).length && gs(f).length == gs(last(steps).diag[index]).length)) {
       return true
     }
   }
-  if(steps.length > 12) {
+  if(steps.length >= min_steps) {
     return true
   }
   return last(steps).diag.some(f => gs(f).length > 0 && ms(f).length > gs(f).length)
 }
-const accept = steps => last(steps).diag[3].length == 4
+const accept = steps => last(steps).diag[3].length == 10
 const output = steps => {
   console.log("---------- Solution ----------")
   steps.forEach((step,index) => {
@@ -33,19 +44,21 @@ const next = (steps,moves) => {
       next_step.diag[next_step.e] = curr_step.diag[curr_step.e].replace(new RegExp('\[' + move.substring(1) + '\]', "g"), '')
       next_step.e = move.charAt(0) == '-' ? curr_step.e - 1 : curr_step.e + 1
       next_step.diag[next_step.e] += move.substring(1)
-      next_step.diag[next_step.e] = next_step.diag[next_step.e].split('').sort().join('')
       steps.push(next_step)
       return steps
     }
   }
   return null
 }
+let min_steps = Number.POSITIVE_INFINITY
 const backtrack = steps => {
   if(reject(steps)) {
     return
   }
   if(accept(steps)) {
+    min_steps = steps.length
     output(steps)
+    return
   }
   let moves = valid_moves.slice()
   let candidate = next(steps,moves)
@@ -56,5 +69,4 @@ const backtrack = steps => {
   }
 }
 
-const p = {diag: ["hl","H","L",""], e: 0}
-backtrack(root(p))
+backtrack(root({diag: ["RTrtCcOP","op","",""], e: 0}))
