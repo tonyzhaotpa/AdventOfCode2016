1d038c865ef8eec415095520bc21a50a2f19b8f2
Tony Yu Zhao <tony.yu.zhao@gmail.com>
2016-12-30 21:27:34 -0500

    Day 14 Solutions (Part 1)
    Removed mutex since only 1 writer

--- adventofcode_day14_part1.rb
+++ adventofcode_day14_part1.rb
@@ -2,20 +2,21 @@
 require 'digest'
 require 'thread'
 
-Item = Struct.new(:md5, :char, :index)
-Buffer = Queue.new
-List = Array.new
-@semaphore = Mutex.new
 Salt = "jlmsuwbz"
+Item = Struct.new(:md5, :char, :index)
+Generated = Array.new
+Key_Indices = Array.new
+Buffer = Queue.new
 
 def produce (integer)
-  return  Digest::MD5.hexdigest Salt + integer.to_s
+  return Digest::MD5.hexdigest Salt + integer.to_s
 end
 
 producer = Thread.new do
   i = 0
   loop do
     md5 = produce i
+    Generated << md5
     if index = /([a-z0-9])\1\1/ =~ md5
       item = Item.new(md5, md5[index], i)
       Buffer << item
@@ -26,20 +27,16 @@ end
 
 consumer = Thread.new do
   loop do
-    @semaphore.synchronize do
-      if List.length >= 64 then
-        puts "64th valid hash: #{List.sort[63]}"
-        exit
-      end
+    if Key_Indices.length >= 64 then
+      puts "64th valid hash: #{Key_Indices.sort[63]}"
+      exit
     end
     item = Buffer.pop
+    sleep(1) until Generated.length > item.index + 1001
     Thread.new do
       (item.index + 1..item.index + 1001).each do |i|
-        md5 = produce i
-        if /(#{item.char})\1\1\1\1/ =~ md5 then
-          @semaphore.synchronize do
-            List << item.index
-          end
+        if /(#{item.char})\1\1\1\1/ =~ Generated[i] then
+          Key_Indices << item.index
           Thread.exit
         end
       end
